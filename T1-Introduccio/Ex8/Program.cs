14c2edd43fac2d7bb3454fa1d565b32f394d51c5
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-02 14:06:11 +0200

    Ex8 Doned

---
+++ T1-Introduccio/Ex8/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string Msg = "Introdueix quants números vols introduir: ";
+            Console.WriteLine(Msg);
+            int n = int.Parse(Console.ReadLine());
+            int suma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Introdueix el número {0}: ", i + 1);
+                int num = int.Parse(Console.ReadLine());
+                suma += num;
+            }
+
+            double mitjana = (double)suma / n;
+            Console.WriteLine("La mitjana dels {0} números és: {1:F2}", n, mitjana);
+        }
+    }
+}
