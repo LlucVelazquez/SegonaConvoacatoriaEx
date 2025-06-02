8fb7373d44c4460b1cbd56efb32a5613cfa74e09
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-02 14:15:17 +0200

    Ex9 Doned

---
+++ T1-Introduccio/Ex9/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex9
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 4;
+            int y = 7;
+            bool p = true;
+            bool q = false;
+
+            // Ex a)
+            bool resultatA = y > x && p == q;
+
+            // Ex c)
+            bool resultatC = !(x * y < 30) && p;
+
+        }
+    }
+}
