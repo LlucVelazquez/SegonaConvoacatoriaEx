1d1a9a8bffc3b63a0496df66368797ff0209affd
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-02 14:23:52 +0200

    Ex10 Doned

---
+++ T1-Introduccio/Ex10/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Ex10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            int z = x + y;
+            Console.WriteLine("El resultat és: {0}", z);
+        }
+    }
+}
