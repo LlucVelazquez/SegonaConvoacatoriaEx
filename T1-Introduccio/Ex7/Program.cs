1818ffa7408ff3ab1c3700e800120410d491cdad
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-02 13:43:59 +0200

    Ex7 Doned

---
+++ T1-Introduccio/Ex7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string Msg = "Introdueix un nombre enter positiu: ";
+            Console.Write(Msg);
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Els divisors parells de {num} són:");
+            for (int i = 2; i <= num; i += 2)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine($"{i} és divisor de {num}");
+                }
+            }
+        }
+    }
+}
