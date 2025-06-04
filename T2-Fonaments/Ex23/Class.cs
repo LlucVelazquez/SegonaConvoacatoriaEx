3d024121030224156f6b4f13784b0df2c5ace400
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-04 18:10:26 +0200

    Ex23 Doned

---
+++ T2-Fonaments/Ex23/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex23
+{
+    public class Class
+    {
+        public static bool IsEven(int num)
+        {
+            return num % 2 == 0;
+        }
+    }
+}
---
+++ T2-Fonaments/Ex23/Test1.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex23
+{
+    [TestClass]
+    public class UtilitatsTests
+    {
+        [TestMethod]
+        public void IsEven_ReturnsTrue_ForEvenNumber()
+        {
+            Assert.IsTrue(Class.IsEven(2));
+            Assert.IsTrue(Class.IsEven(0));
+            Assert.IsTrue(Class.IsEven(-4));
+        }
+
+        [TestMethod]
+        public void IsEven_ReturnsFalse_ForOddNumber()
+        {
+            Assert.IsFalse(Class.IsEven(1));
+            Assert.IsFalse(Class.IsEven(-3));
+            Assert.IsFalse(Class.IsEven(99));
+        }
+    }
+}
