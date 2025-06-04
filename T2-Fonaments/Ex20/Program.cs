3f7ff28910f847f481ade38099f335dd7ccf9787
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-04 13:21:05 +0200

    Ex20 Doned

---
+++ T2-Fonaments/Ex20/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ex20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int tamany = 8;
+            int[] numeros = new int[tamany];
+            for (int i = 0; i < tamany; i++)
+            {
+                Console.Write($"Introdueix el número {i + 1}: ");
+                while (!int.TryParse(Console.ReadLine(), out numeros[i]))
+                {
+                    Console.Write("Entrada no vàlida. Torna-ho a provar: ");
+                }
+            }
+            int maxim = numeros[0];
+            int minim = numeros[0];
+            int suma = 0;
+            foreach (int num in numeros)
+            {
+                if (num > maxim) maxim = num;
+                if (num < minim) minim = num;
+                suma += num;
+            }
+            double mitjana = (double)suma / tamany;
+            Console.WriteLine($"\nEl número més gran és: {maxim}");
+            Console.WriteLine($"El número més petit és: {minim}");
+            Console.WriteLine($"La mitjana és: {mitjana}");
+        }
+    }
+}
