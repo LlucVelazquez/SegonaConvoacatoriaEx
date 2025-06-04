417793fce8208bb5e2fc2d46dcce93b670689f21
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-04 17:25:13 +0200

    Ex21 Doned

---
+++ T2-Fonaments/Ex21/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Ex21
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string Msg = "Introdueix la temperatura en graus Celsius: ";
+            const string Select = "Vols convertir a Fahrenheit (F) o Kelvin (K)? ";
+            double num;
+            Console.WriteLine(Msg);
+            num = double.Parse(Console.ReadLine());
+            Console.WriteLine(Select);
+            string opcio = Console.ReadLine().Trim().ToUpper();
+            switch (opcio)
+            {
+                case "F":
+                    double fahrenheit = CelsiusAFahrenheit(num);
+                    Console.WriteLine($"{num} ºC són {fahrenheit} ºF");
+                    break;
+                case "K":
+                    double kelvin = CelsiusAKelvin(num);
+                    Console.WriteLine($"{num} ºC són {kelvin} K");
+                    break;
+                default:
+                    Console.WriteLine("Opció no vàlida. Escriu 'F' per Fahrenheit o 'K' per Kelvin.");
+                    break;
+            }
+        }
+        public static double CelsiusAFahrenheit(double c)
+        {
+            return c * 9 / 5 + 32;
+        }
+
+        public static double CelsiusAKelvin(double c)
+        {
+            return c + 273.15;
+        }
+    }
+}
