2acf3cb65ff44056e4ef74015b903f267775bda2
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-04 12:43:06 +0200

    Ex15 Doned

---
+++ T2-Fonaments/Ex15/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex15
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string Msg = "Introdueix un número: ";
+            Console.Write(Msg);
+            if (double.TryParse(Console.ReadLine(), out double num))
+            {
+                double resultat = Quadrat(num);
+                Console.WriteLine($"El quadrat de {num} és {resultat}");
+            }
+            else
+            {
+                Console.WriteLine("Entrada no vàlida.");
+            }
+        }
+        public static double Quadrat(double n)
+        {
+            return n * n;
+        }
+    }
+}
