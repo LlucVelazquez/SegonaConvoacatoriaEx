982576e5d27a47398eb8cbe0c82f50066586875d
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-04 18:32:14 +0200

    Ex25 Doned

---
+++ T2-Fonaments/Ex25/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Ex25
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 3, 4, 1 };
+
+            SortArrayAscending(numbers);
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        /// <summary>
+        /// Ordena un array d'enters de manera ascendent.
+        /// </summary>
+        static void SortArrayAscending(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        Swap(ref array[i], ref array[j]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Intercanvia el valor de dos enters.
+        /// </summary>
+        static void Swap(ref int first, ref int second)
+        {
+            int temp = first;
+            first = second;
+            second = temp;
+        }
+    }
+}
