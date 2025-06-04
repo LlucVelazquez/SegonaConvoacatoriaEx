52b1c2a410e9df2fc869bb56fbaa3b97353e2090
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-04 13:00:28 +0200

    Ex17 Doned

---
+++ T2-Fonaments/Ex17/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Ex17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int tamany = 10;
+            int[] numeros = new int[tamany];
+            for (int i = 0; i < tamany; i++)
+            {
+                Console.Write($"Introdueix el número {i + 1}: ");
+                while (!int.TryParse(Console.ReadLine(), out numeros[i]))
+                {
+                    Console.Write("Entrada no vàlida. Torna-ho a provar: ");
+                }
+            }
+            Console.Write("\nIntrodueix el número a buscar: ");
+            if (int.TryParse(Console.ReadLine(), out int aBuscar))
+            {
+                bool trobat = false;
+                Console.WriteLine();
+                for (int i = 0; i < tamany; i++)
+                {
+                    if (numeros[i] == aBuscar)
+                    {
+                        Console.WriteLine($"El número {aBuscar} es troba a la posició {i}.");
+                        trobat = true;
+                    }
+                }
+                if (!trobat)
+                {
+                    Console.WriteLine($"El número {aBuscar} NO es troba a l'array.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada no vàlida.");
+            }
+        }
+    }
+}
