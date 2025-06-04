78b0fb4554ad228035d1e0d6377ecb336a561d5b
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-04 12:49:28 +0200

    Ex16 Doned

---
+++ T2-Fonaments/Ex16/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ex16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = DemanarNumeroEntre1i100();
+            Console.WriteLine($"Has introduït el número {num} correctament!");
+        }
+        static int DemanarNumeroEntre1i100()
+        {
+            int num;
+            bool entradaValida = false;
+
+            do
+            {
+                Console.Write("Introdueix un número entre 1 i 100: ");
+                if (int.TryParse(Console.ReadLine(), out num) && num >= 1 && num <= 100)
+                {
+                    entradaValida = true;
+                }
+                else
+                {
+                    Console.WriteLine("Entrada no vàlida. Torna-ho a intentar.");
+                }
+            } while (!entradaValida);
+
+            return num;
+        }
+    }
+}
