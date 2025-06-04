02ec31c1febec146cc3505d11630227af6870808
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-04 13:15:10 +0200

    Ex19 Doned

---
+++ T2-Fonaments/Ex19/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ex19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("Introdueix un número enter: ");
+            num = int.Parse(Console.ReadLine());
+            {
+                int suma = SumaDigits(Math.Abs(num));
+                Console.WriteLine($"La suma dels dígits de {num} és {suma}");
+            }
+        }
+        static int SumaDigits(int n)
+        {
+            int suma = 0;
+            while (n > 0)
+            {
+                suma += n % 10;
+                n /= 10;
+            }
+            return suma;
+        }
+    }
+}
