9cccdc6ce7a3a45e850d9614e5754fc85a685821
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-06-04 12:10:47 +0200

    Ex10 Doned

---
+++ T2-Fonaments/Ex10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introdueix la cadena: ");
+            string cadena = Console.ReadLine();
+
+            if (TeVuitDigitsILletra(cadena))
+                Console.WriteLine("La cadena és vàlida (8 dígits seguits d’una lletra).");
+            else
+                Console.WriteLine("La cadena NO és vàlida.");
+        }
+
+        static bool TeVuitDigitsILletra(string cadena)
+        {
+            return Regex.IsMatch(cadena, @"^\d{8}[A-Za-z]$");
+        }
+    }
+}
